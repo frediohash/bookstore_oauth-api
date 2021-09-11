952381ae433478d34b09a583ca14e75df5f419f8
fredio.dimyati <fredio.dimyati@dwidasa.com>
2021-09-11 23:36:04 +0700

    Update

--- domain/access_token/service.go
+++ domain/access_token/service.go
@@ -2,9 +2,17 @@ package access_token
 
 import (
 	"github.com/frediohash/bookstore_users-api/utils/errors"
-	"github.com/micro/go-micro/errors"
 )
 
 type Service interface {
 	GetById(string) (*AccessToken, *errors.RestErr)
+}
+type service struct {
+}
+
+func NewService() Service {
+	return &service{}
+}
+func (s *service) GetById(string) (*AccessToken, *errors.RestErr) {
+	return nil, nil
 }
