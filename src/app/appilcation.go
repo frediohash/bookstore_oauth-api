a3be11211f0494aa3acd978e4b28b09d08057ddb
fredio.dimyati <fredio.dimyati@dwidasa.com>
2021-09-12 16:12:23 +0700

    Update

--- src/app/appilcation.go
+++ src/app/appilcation.go
@@ -1,11 +1,6 @@
 package app
 
-import (
-	"github.com/frediohash/bookstore_oauth-api/src/domain/access_token"
-	"github.com/frediohash/bookstore_oauth-api/src/repository/db"
-)
-
 func StartApplication() {
-	dbRepo := db.NewRepository()
-	atService := access_token.NewService(dbRepo)
+	// dbRepo := db.NewRepository()
+	// atService := access_token.NewService(dbRepo)
 }
--- src/domain/access_token/service.go
+++ src/domain/access_token/service.go
@@ -1,10 +1,5 @@
 package access_token
 
-import (
-	"github.com/frediohash/bookstore_oauth-api/src/repository/db"
-	"github.com/frediohash/bookstore_oauth-api/src/utils/errors"
-)
-
 type Service interface {
 	GetById(string) (*AccessToken, *errors.RestErr)
 }
