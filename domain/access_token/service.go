2261e8acedb8fef5f488d913495f9881caff6d35
fredio.dimyati <fredio.dimyati@dwidasa.com>
2021-09-12 17:35:06 +0700

    Update

--- domain/access_token/service.go
+++ domain/access_token/service.go
@@ -1,1 +1,9 @@
 package access_token
+
+import "github.com/frediohash/bookstore_oauth-api/utils/errors"
+
+type Service interface {
+	GetById(string) (*AccessToken, *errors.RestErr)
+}
+type service struct {
+}
