017aeb7f865be410dcb09825d54a4f6ddaa6e712
fredio.dimyati <fredio.dimyati@dwidasa.com>
2021-09-12 17:38:03 +0700

    Update

--- repository/db/db_repository.go
+++ repository/db/db_repository.go
@@ -1,1 +1,20 @@
 package db
+
+import (
+	"github.com/frediohash/bookstore_oauth-api/domain/access_token"
+	"github.com/frediohash/bookstore_oauth-api/utils/errors"
+)
+
+func NewRepository() DbRepository {
+	return &dbRepository{}
+}
+
+type DbRepository interface {
+	GetById(id string) (*access_token.AccessToken, *errors.RestErr)
+}
+type dbRepository struct {
+}
+
+func (r *dbRepository) GetById(id string) (*access_token.AccessToken, *errors.RestErr) {
+	return nil, nil
+}
