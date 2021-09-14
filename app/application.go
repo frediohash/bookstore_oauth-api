4ebdb59c19f772e186550336c506beb821603d1a
fredio.dimyati <fredio.dimyati@dwidasa.com>
2021-09-14 16:55:30 +0700

    Update

--- app/application.go
+++ app/application.go
@@ -1,10 +1,9 @@
 package app
 
 import (
-	"github.com/frediohash/bookstore_oauth-api/domain/access_token"
 	"github.com/frediohash/bookstore_oauth-api/repository/db"
 )
 
 func StartApplication() {
-	atService := access_token.NewService(db.NewRepository())
+	atService := ser(db.NewRepository())
 }
